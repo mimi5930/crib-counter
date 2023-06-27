b83bec106c9ed295fbd2254db805ad2192c3271c
mimi5930 <mikej.miller440@gmail.com>
2023-06-27 15:03:05 -0500

    added testing and hand randomizer

---
+++ src/utilities/tests/play.test.js
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker';
+
+//  Card values should be recorded as such: a,2,3,4,5,6,7,8,9,t,j,k,q
+// Suits should be recorded as: s (spades), d (diamonds), h (hearts), c (clubs)
+
+const suits = faker.string.fromCharacters('sdhc', 5);
+const values = faker.string.fromCharacters('a23456789tjkq', 5);
+
+const hand = createHand(suits, values, 5);
+
+function createHand(suits, values, amount) {
+  let fakeHand = [];
+  for (let i = 0; i < amount; i++) {
+    fakeHand = [...fakeHand, [suits[i], values[i]]];
+  }
+  return fakeHand;
+}
+
+console.log(hand);
